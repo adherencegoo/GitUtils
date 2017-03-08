177fc95e78745b42a73bca2abdf85f23208dcb17
Owen <adherencegoo@gmail.com>
2017-03-09 00:35:51 +0800

    [acceptable] ' of commit and " of description won't be shown

--- gitBranchVerbose.sh
+++ gitBranchVerbose.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Usage: no input
 
+declare -r QUOTE="qwerty" #read only
+
 resetText=`tput sgr0`
 redText=`tput setaf 1`
 greenText=`tput setaf 2`
@@ -14,10 +16,34 @@ whiteBg=`tput setab 7`;
 invertEnable="\e[7m";
 invertDisable="\e[27m"
 
+#========================================================
 #TODO: tmp solution for commit containing apostrophe
-formatString="isHead=%(HEAD) branchName=%(refname:short) sha=%(objectname:short) commitTitle=\"%(contents:subject)\" upstream=%(upstream) push=%(push)"
+# formatString="isHead=%(HEAD) branchName=%(refname:short) sha=%(objectname:short) commitTitle=\"%(contents:subject)\" upstream=%(upstream) push=%(push)"
+formatString="isHead=${QUOTE}%(HEAD)${QUOTE} branchName=${QUOTE}%(refname:short)${QUOTE} sha=${QUOTE}%(objectname:short)${QUOTE} commitTitle=${QUOTE}%(contents:subject)${QUOTE} upstream=${QUOTE}%(upstream)${QUOTE} push=${QUOTE}%(push)${QUOTE}"
+
+#========================================================
+# branchConfigs=`git config --get-regex ^branch.*.*$`;
+# branchBasics=`git for-each-ref --format="$formatString" refs/heads`;
+# echo "branchConfigs:"${branchConfigs[@]}
+# echo "branchConfigs:"${branchConfigs}
+# echo "branchConfigs:"${#branchConfigs[@]}
+# echo
+# echo "branchBasics:"${branchBasics[@]}
+# echo "branchBasics:"${branchBasics}
+# echo "branchBasics:"${#branchBasics[@]}
+# echo
+# exit
+
+#========================================================
 git for-each-ref --shell --format="$formatString" refs/heads | \
 while read entry; do #it must be "entry"
+	echo -n $resetText;
+	printf "=%.0s" `eval echo {1..$(tput cols)}`;#depends on window width
+	
+	#escape characters: to remove ' in values
+	entry=${entry//"'"} #' --> nothing
+	entry=${entry//${QUOTE}/"'"} #${QUOTE} --> '
+	
 	set -f;#disable globbing
 	eval "$entry"
 	set +f;#enable globbing
@@ -31,8 +57,7 @@ while read entry; do #it must be "entry"
 		[[ "$branchName" == dev* ]] && isHead="D";
 	fi
 	headerColor=$resetText;
-	echo -n $resetText;
-	printf "=%.0s" `eval echo {1..$(tput cols)}`;#depends on window width
+
 	
 	#start to echo this branch==============================================
 	#one line vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
@@ -54,7 +79,7 @@ while read entry; do #it must be "entry"
 	[[ ! -z "${push// }" ]] && echo -e $headerColor"\tpush: "$redText"$push";
 		
 	description=`git config branch.$branchName.description`;
-	[[ ! -z "${description// }" ]] && echo -e $headerColor"\tDescription: "$yellowText"$description";
+	[[ ! -z "${description// }" ]] && echo -e $headerColor"\tDescription: "$yellowText$description;
 done
 
 unset isHead;
@@ -62,6 +87,5 @@ unset branchName;
 unset sha;
 unset commitTitle;
 unset description;
-unset remote;
 unset merge;
 unset headerColor;
