7944c8eaf3d5a8b623719ceef15a1de4db549a4b
Owen <adherencegoo@gmail.com>
2017-05-10 00:59:27 +0800

    [NewFeature][gitBranchRank] Allow negative rank $rankVal: 	not set: get value 	0: unset 	1/+1/-1: set to 1/1/-1 	++1/--1: inc/dec by 1 $branchName: 	use HEAD by default

--- gitBranchRank.sh
+++ gitBranchRank.sh
@@ -3,22 +3,29 @@
 	# $rankVal:
 		# not set: get value
 		# 0: unset
-		# 1: set to 1
-		# ++/+1: inc by 1
-		# --/-1: dec by 1
+		# 1/+1/-1: set to 1/1/-1
+		# ++1/--1: inc/dec by 1
 	# $branchName:
-		# not set: use HEAD branch
-	
+		# use HEAD by default
+
+declare -r resetText=`tput sgr0`;
+declare -r redText=`tput setaf 1`;
+declare -r ACTION_SHOW=0;
+declare -r ACTION_UNSET=1;
+declare -r ACTION_SET=2;
+declare -r ACTION_ADJUST=3;
 #handle inputs==============================================
-unset rankVal;
-unset branchName;
+currentAction=$ACTION_SHOW;
 for arg in $@; do 
-	if [[ $arg == "++" ]]; then 
-		rankVal="+1";
-	elif [[ $arg == "--" ]]; then 
-		rankVal="-1";
-	elif [[ $arg =~ ^[+-]?[0-9]+$ ]]; then 
-		rankVal=$arg;
+	if [[ $arg -eq 0 ]]; then
+		currentAction=$ACTION_UNSET;
+	elif [[ $arg =~ ^[+-]?[1-9][0-9]*$ ]]; then #1 or +1 or -1
+		currentAction=$ACTION_SET;
+		rankVal=$(( $arg ));#transform into numerical value
+	elif ([[ ${arg:0:2} == "++" ]] || [[ ${arg:0:2} == "--" ]]) && [[ ${arg:2} =~ ^[1-9][0-9]*$ ]]; then #++1 or --1
+		#NOTE: pattern ^(?:--|[+]{2})[1-9][0-9]*$ passed on online website, but not work in bash ...
+		currentAction=$ACTION_ADJUST;
+		rankVal=$(( ${arg:1} ));#remove first character (or, --1 is treated as 1), and transform into numerical value
 	else
 		branchName=$arg;
 	fi
@@ -29,32 +36,40 @@ done
 #TODO: if branchName is set but not valid, treat it as regex
 
 #debug:
+# echo currentAction: $currentAction
 # echo rankVal: $rankVal
 # echo branchName: $branchName
 
-if [[ -z $rankVal ]]; then #not set, display it
-	echo `git config branch.$branchName.rank`;
-elif [[ 0 -eq $rankVal ]]; then #numerical comparison, equal to 0: unset rank
-	git config --unset branch.$branchName.rank
-	echo "Unset rank of $branchName";
-	
-	#remove section if it's empty after unsetting this setting
-	sectionContent=`git config --get-regex "^branch.$branchName.*"`;
-	if [[ -z ${sectionContent// } ]]; then
-		git config --remove-section branch.$branchName
-		echo "Remove empty section: branch.$branchName";
-	fi
-elif [[ $rankVal =~ ^[0-9]+$ ]]; then #not start with + or -: set rank
-	git config branch.$branchName.rank $(($rankVal)) #numerical form
-	echo "Rank of $branchName: $rankVal";
-else #inc or dec
-	newRank=$(( `git config branch.$branchName.rank` + $rankVal ));
-	[[ $newRank -lt 0 ]] && newRank=0; #newRank must >= 0 and contains no +-
-	eval $(basename $0) $branchName $newRank; #recursive call
-fi
+case $currentAction in
+	$ACTION_SHOW ) #------------------------------------------------------------------------
+		echo `git config branch.$branchName.rank`;
+		;;
+	$ACTION_UNSET ) #------------------------------------------------------------------------
+		git config --unset branch.$branchName.rank
+		echo "Unset rank of $branchName";
 
-unset sectionContent;
-unset arg;
-unset newRank;
-unset rankVal;
-unset branchName;
+		#remove section if it's empty after unsetting this setting
+		sectionContent=`git config --get-regex "^branch.$branchName.*"`;
+		if [[ -z ${sectionContent// } ]]; then
+			git config --remove-section branch.$branchName
+			echo "Remove empty section: branch.$branchName";
+		fi
+		;;
+	$ACTION_SET ) #------------------------------------------------------------------------
+		git config branch.$branchName.rank $rankVal;
+		echo "Rank of $branchName: $rankVal";
+		;;
+	$ACTION_ADJUST ) #------------------------------------------------------------------------
+		origRank=`git config branch.$branchName.rank`;
+		newRank=$(( $rankVal + $origRank ));
+		if [[ $newRank -eq 0 ]]; then
+			eval $(basename $0) $branchName 0; #recursive call
+		else
+			git config branch.$branchName.rank $newRank;
+			echo "Rank of $branchName: $newRank";
+		fi
+		;;
+	* ) #------------------------------------------------------------------------
+		echo -e $redText"Unexpected action:"$currentAction;
+		;;
+esac
